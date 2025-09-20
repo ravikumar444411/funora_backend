1ed66362c8a6e73b445db2c894dff5a4ca01cb13
ravikumar444411 <ravikumar444411@gmail.com>
2025-09-20 10:19:31 +0530

    add deeplink concept

--- src/server.js
+++ src/server.js
@@ -8,6 +8,32 @@ app.get("/", (req, res) => {
   res.send("Hello, World! 9 🌎🚀");
 });
 
+
+app.get("/deeplink/event/:id", (req, res) => {
+  const eventId = req.params.id;
+  const packageName = "com.funora.app"; // replace with your package name
+
+  // Construct deep link (custom scheme or app link)
+  const deepLink = `funora://deeplink/event/${eventId}`;
+
+  // Try opening the app
+  const html = `
+    <html>
+      <head>
+        <meta http-equiv="refresh" content="0; url=${deepLink}">
+      </head>
+      <body>
+        <script>
+          setTimeout(() => {
+            window.location.href = "https://play.google.com/store/apps/details?id=${packageName}";
+          }, 1500);
+        </script>
+      </body>
+    </html>
+  `;
+  res.send(html);
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running new on http://localhost:${PORT}`);
 });
